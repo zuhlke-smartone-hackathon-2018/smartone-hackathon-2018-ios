fca47cd10ca01ab6f22ec2a622f7bac788b7fa50
Kevin Lo <kevin@kev.hk>
2018-10-20 15:25:31 +0800

    Initial Commit

---
+++ SMT Hackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SMT Hackathon
+//
+//  Created by Kevin Lo on 20/10/2018.
+//  Copyright © 2018 Zuhlke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
